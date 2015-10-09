bcffe46324c075f24980a874b0c873c875c983fe
dapeng.fu <dapeng@dapengfus-MacBook-Pro.local>
2015-10-09 09:33:41 +0800

    update

--- src/MyApi/tests/default_test.go
+++ src/MyApi/tests/default_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"runtime"
 	"path/filepath"
-	_ "Users/dapeng/Desktop/temp/GoTest/MyApi/routers"
+	_ "MyApi/routers"
 
 	"github.com/astaxie/beego"
 	. "github.com/smartystreets/goconvey/convey"
